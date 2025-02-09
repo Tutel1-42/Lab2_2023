a702cae0ce0e112ece8d4a2c979cf77ed57d361d
Tutel1-42 <ice.sajakorn@gmail.com>
2025-02-09 10:24:29 +0700

    BaseCompetitor done

---
+++ src/logic/unit/BaseCompetitor.java
@@ -0,0 +1,63 @@
+package logic.unit;
+
+public class BaseCompetitor {
+	private String name;
+	private int power;
+	private int hp;
+
+	// methods
+
+	public BaseCompetitor(String name) {
+		this.setHp(5);
+		this.setPower(3);
+		this.setName(name);
+
+	}
+
+	public BaseCompetitor(String name, int hp, int power) {
+		this.setName(name);
+		this.setHp(hp);
+		this.setPower(power);
+	}
+	
+	public void attack(BaseCompetitor enemy) {
+		enemy.setHp(enemy.getHp() - this.getPower());
+	}
+	
+	public String getType() {
+		return "BaseCompetitor";
+	}
+
+	// setters & getters
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		if (power < 1) {
+			this.power = 1;
+		} else
+			this.power = power;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		if (hp < 0) {
+			this.hp = 0;
+		} else
+			this.hp = hp;
+	}
+
+}
